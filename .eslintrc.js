30545099c424ab75ac1fe789fa21abd63d3e0c03
高博 <gaobo_1994@126.com>
2021-01-06 23:14:27 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    require.resolve('@umijs/fabric/dist/eslint'),
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: 'babel-eslint',
+  plugins: ['react'],
+  globals: {
+    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
+    page: true,
+    REACT_APP_ENV: true,
+  },
+  rules: {
+    'react/prop-types': ['off'],
+  },
+};
